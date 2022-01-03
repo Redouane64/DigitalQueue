923e66ef7b80129a59023cda5abd524f0ca27d3f
redouane <redouane.sobaihi@live.com>
2022-01-03 19:32:41 +0700

    feat(): display account info in profile page

--- src/DigitalQueue.Web/Areas/Accounts/Pages/Profile.cshtml.cs
+++ src/DigitalQueue.Web/Areas/Accounts/Pages/Profile.cshtml.cs
@@ -1,15 +1,31 @@
+using DigitalQueue.Web.Users.Dtos;
+using DigitalQueue.Web.Users.Queries;
+
+using MediatR;
+
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DigitalQueue.Web.Areas.Accounts.Pages;
 
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme)]
 public class ProfileModel : PageModel
 {
-    public void OnGet()
-    {
+    private readonly IMediator _mediator;
 
+    public ProfileModel(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public UserDto? Profile { get; set; }
+    
+    public async Task OnGetAsync()
+    {
+        Profile = await _mediator.Send(new GetProfileRequest(User));
     }
 
     public async Task<IActionResult> OnPostAsync()
