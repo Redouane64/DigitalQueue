e3bd148148f20c3f84645c9064b4b842d4d2863c
redouane <redouane.sobaihi@live.com>
2022-02-20 00:29:22 +0700

    feat(): create user using transaction

--- src/DigitalQueue.Web/Areas/Accounts/Commands/CreateAccountCommand.cs
+++ src/DigitalQueue.Web/Areas/Accounts/Commands/CreateAccountCommand.cs
@@ -38,79 +38,90 @@ public class CreateAccountCommand : IRequest<AccessTokenDto?>
     {
         private readonly UserManager<User> _userManager;
         private readonly JwtTokenService _tokenService;
+        private readonly DigitalQueueContext _context;
         private readonly ILogger<CreateAccountCommandHandler> _logger;
 
         public CreateAccountCommandHandler(
             UserManager<User> userManager, 
             JwtTokenService tokenService,
+            DigitalQueueContext context,
             ILogger<CreateAccountCommandHandler> logger)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _context = context;
             _logger = logger;
         }
 
         public async Task<AccessTokenDto?> Handle(CreateAccountCommand request, CancellationToken cancellationToken)
         {
-            var user = new User()
+            await using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+            try
             {
-                Email = request.Email,
-                FullName = request.FullName,
-                UserName = request.Email.Split("@").First()
-            };
+                var user = new User()
+                {
+                    Email = request.Email,
+                    FullName = request.FullName,
+                    UserName = request.Email.Split("@").First()
+                };
             
-            var createUser = await _userManager.CreateAsync(
-                user, request.Password
-            );
+                var createUser = await _userManager.CreateAsync(
+                    user, request.Password
+                );
 
-            if (!createUser.Succeeded)
+                if (!createUser.Succeeded)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    
+                    _logger.LogWarning("Unable to create user: {0}",
+                        createUser.Errors.Select(e => e.Description).First());
+                    return null;
+                }
+
+                request.Roles = new[] {RoleDefaults.User};
+                var assignRole = await _userManager.AddToRolesAsync(user, request.Roles);
+                if (!assignRole.Succeeded)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    
+                    _logger.LogWarning("Unable to set user role: {0}",
+                        assignRole.Errors.Select(e => e.Description).First());
+                    return null;
+                }
+
+                Claim[] identityClaims = {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id)
+                };
+                var roleClaims = request.Roles.Select(
+                    role => new Claim(ClaimTypes.Role, role)
+                );
+
+                var assignClaims = await _userManager.AddClaimsAsync(user, identityClaims.Concat(roleClaims));
+                if (!assignClaims.Succeeded)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    
+                    _logger.LogWarning("Unable to set user claims: {0}",
+                        assignClaims.Errors.Select(e => e.Description).First());
+                    return null;
+                }
+
+                await transaction.CommitAsync(cancellationToken);
+
+                var (token, refreshToken) = await _tokenService.GenerateToken(
+                    identityClaims,
+                    user
+                );
+
+                return new AccessTokenDto(token, refreshToken);
+            }
+            catch (Exception exception)
             {
-                _logger.LogWarning("Unable to create user: {0}",
-                    createUser.Errors.Select(e => e.Description).First());
-                return null;
+                _logger.LogError(exception, "Unable to create user");
             }
 
-            request.Roles = new[] {RoleDefaults.User};
-            var assignRole = await _userManager.AddToRolesAsync(user, request.Roles);
-            if (!assignRole.Succeeded)
-            {
-                // at this point we failed.
-                _logger.LogWarning("Unable to set user role: {0}",
-                    assignRole.Errors.Select(e => e.Description).First());
-
-                // clean up invalid user
-                await _userManager.DeleteAsync(user);
-                
-                return null;
-            }
-
-            Claim[] identityClaims = {
-                new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.NameIdentifier, user.Id)
-            };
-            var roleClaims = request.Roles.Select(
-                role => new Claim(ClaimTypes.Role, role)
-            );
-
-            var assignClaims = await _userManager.AddClaimsAsync(user, identityClaims.Concat(roleClaims));
-            if (!assignClaims.Succeeded)
-            {
-                // at this point we failed
-                _logger.LogWarning("Unable to set user claims: {0}",
-                    assignClaims.Errors.Select(e => e.Description).First());
-
-                // clean up invalid user
-                await _userManager.DeleteAsync(user);
-                
-                return null;
-            }
-
-            var (token, refreshToken) = await _tokenService.GenerateToken(
-                identityClaims,
-                user
-            );
-
-            return new AccessTokenDto(token, refreshToken);
+            return null;
         }
     }
 }
