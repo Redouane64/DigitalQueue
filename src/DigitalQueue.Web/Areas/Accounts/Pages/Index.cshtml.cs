d4214b22bcfbf189f6e60cb7a75a9d205fddea26
redouane <redouane.sobaihi@live.com>
2022-02-19 20:19:14 +0700

    fix(): require auth for user list page

--- src/DigitalQueue.Web/Areas/Accounts/Pages/Index.cshtml.cs
+++ src/DigitalQueue.Web/Areas/Accounts/Pages/Index.cshtml.cs
@@ -3,10 +3,13 @@ using DigitalQueue.Web.Areas.Accounts.Queries;
 
 using MediatR;
 
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DigitalQueue.Web.Areas.Accounts.Pages;
 
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Policy = "Admin")]
 public class Index : PageModel
 {
     private readonly IMediator _mediator;
