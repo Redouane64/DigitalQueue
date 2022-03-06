c72eb331818639b4daa99b18effd8852ab29962f
redouane <redouane.sobaihi@live.com>
2022-03-07 00:31:27 +0700

    feat(): delete dead code

--- src/DigitalQueue.Web/Areas/Dashboard/Pages/Index.cshtml.cs
+++ src/DigitalQueue.Web/Areas/Dashboard/Pages/Index.cshtml.cs
@@ -1,8 +1,3 @@
-using DigitalQueue.Web.Areas.Courses.Dtos;
-using DigitalQueue.Web.Areas.Courses.Queries;
-
-using MediatR;
-
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,17 +7,12 @@ namespace DigitalQueue.Web.Areas.Dashboard.Pages;
 [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Policy = "Admin")]
 public class Index : PageModel
 {
-    private readonly IMediator _mediator;
 
-    public Index(IMediator mediator)
+    public Index()
     {
-        _mediator = mediator;
     }
     
-    public IEnumerable<CourseDto> Courses { get; set; }
-
     public async Task OnGet()
     {
-        Courses = await this._mediator.Send(new GetCoursesQuery());
     }
 }
