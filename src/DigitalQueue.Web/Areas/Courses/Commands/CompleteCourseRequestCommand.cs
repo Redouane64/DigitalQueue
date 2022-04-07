ea8f096b1b8b586d1d1ce9ba54f454da536c2244
redouane <redouane.sobaihi@live.com>
2022-04-08 02:04:07 +0700

    feat(): implement handlers for create and update course request

---
+++ src/DigitalQueue.Web/Areas/Courses/Commands/CompleteCourseRequestCommand.cs
@@ -0,0 +1,59 @@
+using DigitalQueue.Web.Data;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalQueue.Web.Areas.Courses.Commands;
+
+public class CompleteCourseRequestCommand : IRequest
+{
+    public string RequestId { get; }
+    public string UserId { get; }
+
+    public CompleteCourseRequestCommand(string requestId, string userId)
+    {
+        RequestId = requestId;
+        UserId = userId;
+    }
+    
+    public class CompleteCourseRequestCommandHandler : IRequestHandler<CompleteCourseRequestCommand>
+    {
+        private readonly DigitalQueueContext _context;
+        private readonly ILogger<CompleteCourseRequestCommandHandler> _logger;
+
+        public CompleteCourseRequestCommandHandler(DigitalQueueContext context, ILogger<CompleteCourseRequestCommandHandler> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        
+        public async Task<Unit> Handle(CompleteCourseRequestCommand request, CancellationToken cancellationToken)
+        {
+            await using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+            try
+            {
+                var courseRequest =
+                    await _context.Requests.FirstOrDefaultAsync(r =>
+                        r.Id == request.RequestId && r.CreatorId == request.UserId, cancellationToken: cancellationToken);
+
+                if (courseRequest is null)
+                {
+                    return Unit.Value;
+                }
+                
+                courseRequest.Completed = true;
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                _logger.LogError(
+                    e, "Unable to modify request {}", 
+                    request.RequestId);
+            }
+            
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/DigitalQueue.Web/Areas/Courses/Commands/CreateCourseRequestCommand.cs
@@ -0,0 +1,72 @@
+using DigitalQueue.Web.Data;
+using DigitalQueue.Web.Data.Entities;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalQueue.Web.Areas.Courses.Commands;
+
+public class CreateCourseRequestCommand : IRequest
+{
+    public string CourseId { get; }
+    public string UserId { get; }
+
+    public CreateCourseRequestCommand(string courseId, string userId)
+    {
+        CourseId = courseId;
+        UserId = userId;
+    }
+    
+    public class CreateCourseRequestCommandHandler : IRequestHandler<CreateCourseRequestCommand>
+    {
+        private readonly DigitalQueueContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<CreateCourseRequestCommandHandler> _logger;
+
+        public CreateCourseRequestCommandHandler(DigitalQueueContext context, UserManager<User> userManager, ILogger<CreateCourseRequestCommandHandler> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+        
+        public async Task<Unit> Handle(CreateCourseRequestCommand request, CancellationToken cancellationToken)
+        {
+            await using var transaction = await this._context.Database.BeginTransactionAsync(cancellationToken);
+            try
+            {
+                var course =
+                    await this._context.Courses.FirstOrDefaultAsync(c => c.Id == request.CourseId, cancellationToken);
+
+                if (course is null)
+                {
+                    return Unit.Value;
+                }
+
+                var user = await _userManager.FindByIdAsync(request.UserId);
+
+                if (user is null)
+                {
+                    return Unit.Value;
+                }
+
+                var courseRequest = new Request { Course = course, Creator = user };
+                _context.Add(courseRequest);
+
+                await _context.SaveChangesAsync(cancellationToken);
+                await transaction.CommitAsync();
+            }
+            catch (Exception e)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+                _logger.LogError(
+                    e, "Unable to create request for course {} with user {}", 
+                    request.CourseId, request.UserId);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
