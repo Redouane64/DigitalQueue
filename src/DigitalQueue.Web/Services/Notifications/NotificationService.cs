d63819457fb6a4b860383a45db72286f6ca51dbb
redouane <redouane.sobaihi@live.com>
2022-05-02 21:11:14 +0700

    feat(): update notification template and logic

--- src/DigitalQueue.Web/Services/Notifications/NotificationService.cs
+++ src/DigitalQueue.Web/Services/Notifications/NotificationService.cs
@@ -19,7 +19,7 @@ public class NotificationService
         switch (notification.Payload)
         {
             case VerificationToken verificationToken:
-                await this._mailService.SendEmailConfirmationCode(verificationToken.Email, verificationToken.Token);
+                await this._mailService.SendAuthenticationCode(verificationToken.Email, verificationToken.Token);
                 return;
             case PasswordResetToken passwordResetToken:
                 await this._mailService.SendPasswordResetCode(passwordResetToken.Email, passwordResetToken.Token);
